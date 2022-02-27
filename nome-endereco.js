b5ff1671e8ed5eb610bf25c949e519393c0a222e
Andrew <andrew_oliveira@ymail.com>
2022-02-27 01:52:35 -0300

    arquivos da aula presencial

---
+++ nome-endereco.js
@@ -0,0 +1,6 @@
+var nome = 'Andrew Wanderley Oliveira';
+var idade = '26 anos';
+var endereco = 'Estrada das Pitas, 799 - Jd Líbano, Barueri';
+console.log ('Bom dia / Boa tarde / Boa noite');
+console.log ('Meu nome e', nome,',  tenho', idade);
+console.log ('Moro na ', endereco)
---
+++ operacoesmat.js
@@ -0,0 +1,14 @@
+var n1 = 4
+var n2 = 2
+var soma, menos, dividir, vezes, resto;
+soma = n1 + n2;
+menos = n1 - n2;
+dividir = n1 / n2;
+vezes = n1 * n2;
+resto = n1 % n2;
+console.log (n1, 'e', n2)
+console.log (n1 ,' mais ',n2, ' e igual a ',soma);
+console.log (n1 ,' menos',n2,' e igual a ',menos);
+console.log (n1,' vezes',n2, ' e igual a ', vezes);
+console.log (n1, ' dividido por ',n2,' e igual a  ', dividir , 'com resto',resto );
+console.log ('Fim');
